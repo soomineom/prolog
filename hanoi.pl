1d5490f91540dd57110da1efb9bf9c0adf7c887c
e5718 <e5718@naver.com>
2020-06-24 00:05:45 +0900

    1

---
+++ hanoi.pl
@@ -0,0 +1,19 @@
+hanoi(N) :- func(N,1,2,3).
+
+func(0,_,_,_) :- !.
+
+func(N,A,B,C) :-
+        M is N-1,
+        func(M,A,C,B),
+        inform(M,A,B),
+        func(M,C,B,A).
+
+inform(M,F,T) :-
+        K is M+1,
+        write(K),
+        write('->'),
+        write([F,T]),
+        nl.
+
+
+
